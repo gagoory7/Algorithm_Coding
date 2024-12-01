422458ec3e18d9b9ffa2f0e55741c8272ba6a6d8
gagoory7 <163074222+gagoory7@users.noreply.github.com>
2024-12-01 22:41:29 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 312 ms, Memory: 59740 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -5,19 +5,18 @@ n, m = map(int, sys.stdin.readline().split())
 str_dic = {}
 
 for _ in range(n):
-    tmp = sys.stdin.readline().strip()  # 줄바꿈 제거
-    if len(tmp) >= m:  # 길이 조건 확인
+    tmp = sys.stdin.readline().strip()  
+    if len(tmp) >= m:  
         if tmp in str_dic:
             str_dic[tmp] += 1
         else:
             str_dic[tmp] = 1
 
-# 정렬: (빈도수 내림차순, 길이 내림차순, 알파벳 오름차순)
 sorted_dict = sorted(
     str_dic.items(),
     key=lambda item: (-item[1], -len(item[0]), item[0])
 )
 
-# 출력
+
 for i, _ in sorted_dict:
     print(i)
